5a2d5d2909efa4ff462586491d28a6fc101d4e68
Jason Oscar <jhoscar@gmail.com>
2019-01-28 22:35:11 -0500

    Initial refactor commit changing package imports.

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/GoBootcamp/clirescue/trackerapi"
+	"github.com/jhoscar1/clirescue/trackerapi"
 	"github.com/codegangsta/cli"
 )
 
--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/jhoscar1/clirescue/cmdutil"
+	"github.com/jhoscar1/clirescue/user"
 )
 
 var (
